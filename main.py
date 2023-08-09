fe32726d88edf6ab5f8cd2004e2001194b5b7909
SasiNisa <127511379+SasiNisa@users.noreply.github.com>
2023-08-08 23:26:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+
 import pickle
 import streamlit as st
 import requests
+from pytube import YouTube
+import webbrowser  # Import the webbrowser module
+import itertools
 
+
+
+# Function to fetch YouTube video URL
+def fetch_youtube_url(movie_name):
+    query = f"{movie_name} movie trailer"
+    url = f"https://www.googleapis.com/youtube/v3/search?key=AIzaSyAsMzRKv4pe5eAeBSJxz8U0mMDu8T9F3Mw&q={query}&maxResults=1&type=video"
+    response = requests.get(url)
+    data = response.json()
+    video_id = data['items'][0]['id']['videoId']
+    youtube_url = f"https://www.youtube.com/watch?v={video_id}"
+    return youtube_url
+
+# Function to open YouTube video in a separate window
+def open_youtube_video(video_url):
+    webbrowser.open_new_tab(video_url)
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=bf1d8a5f0f007057b89e0f52a5d0d778&language=en-US".format(movie_id)  #8265bd1679663a7ea12ac168da84d2e8
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
@@ -43,11 +62,14 @@ if st.button('Show Recommendation'):
     # Create a list of column elements
     columns = st.columns(num_recommendations)
 
-    # Loop through each column and display the recommendations
     for i in range(num_recommendations):
         with columns[i]:
             st.text(recommended_movie_names[i])
             st.image(recommended_movie_posters[i])
+            youtube_url = fetch_youtube_url(recommended_movie_names[i])
+            st.write(f"[Watch the Movie here]({youtube_url})")
+
+
 
 
 
